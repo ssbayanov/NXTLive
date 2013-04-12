50f7bb8a15b92268ddd85749a34e8a88028c8961
Станислав <>
2013-04-12 23:04:12 +0800

    Make easy http server. Give index in assets

--- src/com/example/nxtlive/httpService.java
+++ src/com/example/nxtlive/httpService.java
@@ -3,12 +3,17 @@ package com.example.nxtlive;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.StringWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.os.Handler;
 import android.util.Log;
 
@@ -24,8 +29,10 @@ public class httpService {
 
 	private static final int TCP_SERVER_PORT = 8080;
 
-	public httpService(Context context, Handler handler) {
+	private Context mainContext = null;
 
+	public httpService(Context context, Handler handler) {
+		mainContext = context;
 		mHandler = handler;
 	}
 
@@ -62,33 +69,51 @@ public class httpService {
 		public void run() {
 			try {
 				Log.d(TAG, "mHttpThread running");
-				s = ss.accept();
-				Log.d(TAG, "Try create mHttpThread s5");
-				in = new BufferedReader(new InputStreamReader(
-						s.getInputStream()));
 
-				out = new BufferedWriter(new OutputStreamWriter(
-						s.getOutputStream()));
-				// Keep listening to the InputStream while connected
 				while (true) {
+					if (s != null) {
+						s.close();
+					}
+					s = ss.accept();
+					Log.d(TAG, "Try create mHttpThread s5");
+					in = new BufferedReader(new InputStreamReader(
+							s.getInputStream()));
 
-					// receive a message
-					String incomingMsg = in.readLine()
-							+ System.getProperty("line.separator");
-					Log.i("TcpServer", "received: " + incomingMsg);
-					// mHandler.obtainMessage("received: " +
-					// incomingMsg).sendToTarget();
-					// send a message
-					String outgoingMsg = "goodbye from port " + TCP_SERVER_PORT
-							+ System.getProperty("line.separator");
+					out = new BufferedWriter(new OutputStreamWriter(
+							s.getOutputStream()));
+					// Keep listening to the InputStream while connected
+					List<String> query = new ArrayList<String>();
+
+					String buf;
+					do {
+						buf = in.readLine();
+						query.add(buf);
+						Log.i("TcpServer", "received: " + buf);
+					} while (buf.length() > 2);
+					Log.i(TAG, "Readin query finish");
+					String outgoingMsg = "HTTP/1.0 200 Ok\r\nPragma: no-cache\r\nExpires: Thu, 01 Jan 1970 00:00:01 GMT\r\nContent-Type: text/html; charset=\"utf-8\"\r\n\r\n";//<html><head><title>Панель управления</title></head><body><h1>Панель управления</h1></body></html>";
 					out.write(outgoingMsg);
+					AssetManager am = mainContext.getAssets();
+
+					InputStream input = am.open("index.html");
+
+					int size = input.available();
+					byte[] buffer = new byte[size];
+					input.read(buffer);
+					input.close();
+
+					// byte buffer into a string
+					String text = new String(buffer);
+
+					out.write(text);
+					
+
 					out.flush();
-					Log.i("TcpServer", "sent: " + outgoingMsg);
+					// Log.i("TcpServer", "sent: " + outgoingMsg);
 					// mHandler.obtainMessage("sent: " +
 					// outgoingMsg).sendToTarget();
 
 					// SystemClock.sleep(5000);
-
 				}
 			} catch (IOException e) {
 				e.printStackTrace();
